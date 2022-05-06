5734b4f655a85c915e426ccb0a2e0335433b14ba
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-06 14:36:53 -0500

    lecture follow along

--- src/main/resources/movies-create-script.sql
+++ src/main/resources/movies-create-script.sql
@@ -1,26 +1,63 @@
-# 1. go get the json file from glitch
-# 2. copypasta into a new json file under /resources
+CREATE DATABASE IF NOT EXISTS movies_db;
 
-# --> You may need to establish a connection to your localhost db here
+USE movies_db;
+#DROP THE TABLE WITH THE MOST DEPENDENCIES FIRST
+DROP TABLE IF EXISTS movie_genre;
+DROP TABLE IF EXISTS movie_actor;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS directors;
 
-# 3. create the movies_db
+CREATE TABLE IF NOT EXISTS directors
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
 
-# 4. use the movies_db
+CREATE TABLE IF NOT EXISTS genres
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32),
+    PRIMARY KEY (id)
+);
 
-# 5. drop the table(s) to which no other tables are dependent (none at first)
+CREATE TABLE IF NOT EXISTS movies
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title       VARCHAR(255) NOT NULL,
+    year        CHAR(4)      NOT NULL,
+    plot        TEXT,
+    rating      CHAR(1)      NOT NULL,
+    poster      TEXT,
+    director_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (director_id) REFERENCES directors (id)
+);
 
-# 6. map the json movie properties to movies table columns
-# --> start with just a movies table with all the columns found in the movie json properties
+CREATE TABLE IF NOT EXISTS movie_genre
+(
+    movie_id INT UNSIGNED NOT NULL,
+    genre_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id)
+);
 
-# 6a. Run the script to make sure it works
+CREATE TABLE IF NOT EXISTS actors
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
-# 7. refactor to extract the directors to a new table with just an id and name
-# --> change the movies table to reference the directors table via Foreign Key
-# --> now that movies is dependent on directors, you need to move directors above movies in the script
-
-# 8. Go add DROP IF EXIST statements for movies and directors
-
-# 9. RUN IT!
+CREATE TABLE IF NOT EXISTS movie_actor
+(
+    movie_id INT UNSIGNED NOT NULL,
+    actor_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (actor_id) REFERENCES actors (id)
+);
 
 
 
