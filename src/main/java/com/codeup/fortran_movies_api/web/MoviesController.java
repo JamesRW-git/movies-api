0a0b6bbc4735a78a4e2f909aa6bbefd58fe88878
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-10 18:28:05 -0500

    add read functionality

--- src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
+++ src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
@@ -11,18 +11,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/movies")
 public class MoviesController {
-    //Temporary backing field to give us something to work with, will be removed
-    private List<Movie> sampleMovies = setMovies();
 
     private final MoviesRepository moviesRepository;
 
     public MoviesController(MoviesRepository moviesRepository) {
         this.moviesRepository = moviesRepository;
-    }
-
-    @GetMapping
-    public Movie one() {
-        return moviesRepository.getById(1);
     }
 
     @GetMapping("all")
@@ -32,25 +25,19 @@ public class MoviesController {
 
     @GetMapping("{id}")
     public Movie getById(@PathVariable int id) {
-        return sampleMovies.stream().filter((movie) -> {
-                    return movie.getId() == id;
-        })
-                .findFirst()
-                .orElse(null);
+        return moviesRepository.findById(id).orElse(null);
     }
 
     //@GetMapping("search/{title}")
     //public Movie getByTitle(@PathVariable String title){
     @GetMapping("search")
-    public Movie getByTitle(@RequestParam String title){
-        Movie movieToReturn = null;
+    public List<Movie> getByTitle(@RequestParam("title") String title){
+        return moviesRepository.findByTitle(title);
+    }
 
-        for (Movie movie : sampleMovies){
-            if(movie.getTitle().equals(title)){
-                movieToReturn = movie;
-            }
-        }
-        return movieToReturn;
+    @GetMapping("search/year") // api/movies/search/year
+    public List<Movie> getByYearRange(@RequestParam("startYear") int startYear, @RequestParam("endYear") int endYear){
+        return moviesRepository.findByYearRange(startYear, endYear);
     }
 
     @PostMapping
@@ -63,34 +50,4 @@ public class MoviesController {
         moviesRepository.saveAll(moviesToAdd);
     }
 
-    private List<Movie> setMovies() {
-        List<Movie> movies = new ArrayList<>();
-
-        movies.add(new Movie(2, "Pulp Fiction", "1994",/* "Quentin Tarantino",
-                "Samuel L. Jackson, Uma Thurman, Bruce Willis, John Travolta, Ving Rhames",
-                "action, drama, suspense, cult classic, crime",*/
-                "Vincent Vega (John Travolta) and Jules Winnfield (Samuel L. Jackson) are hitmen with a penchant " +
-                        "for philosophical discussions. In this ultra-hip, multi-strand crime movie, their storyline is " +
-                        "interwoven with those of their boss, gangster Marsellus Wallace (Ving Rhames) ; his actress " +
-                        "wife, Mia (Uma Thurman) ; struggling boxer Butch Coolidge (Bruce Willis) ; master fixer Winston" +
-                        " Wolfe (Harvey Keitel) and a nervous pair of armed robbers, \"Pumpkin\" (Tim Roth) " +
-                        "and \"Honey Bunny\" (Amanda Plummer)."
-        ));
-
-        movies.add(new Movie(3, "Birdemic", "2010",/* "James Nguyen", "Alan Bagh, " +
-                "Whitney Moore, Tippi Hedren", "horror, thriller",*/
-                "In the town of Half Moon Bay California, there's an epidemic on the " +
-                        "horizon: a Birdemic. All the birds are attacking humanity for their devastating damage they're " +
-                        "inflicting on the Earth: they're going after gas stations and people's cars, and the birds have " +
-                        "something the humans will never see coming: Bird-Acid."
-        ));
-        movies.add(new Movie(1, "The Big Lebowski",
-                "1995", /*"The Cohen Bros",
-                "Jeff Bridges, John Goodman, Steve Buscemi",
-                "comedy, drama?",*/
-                "the dude just wanted to relax and go bowling"
-        ));
-
-        return movies;
-    }
 }
