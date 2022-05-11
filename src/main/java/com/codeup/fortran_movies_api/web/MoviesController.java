23fc3478978fb80c72b0fbc1d08fc391379327ca
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-11 09:22:33 -0500

    add error handling for delete functionality

--- src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
+++ src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
@@ -2,8 +2,13 @@ package com.codeup.fortran_movies_api.web;
 
 import com.codeup.fortran_movies_api.data.Movie;
 import com.codeup.fortran_movies_api.data.MoviesRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,8 +56,12 @@ public class MoviesController {
     }
 
     @DeleteMapping("{id}") // /api/movies/{id}
-    public void deleteById(@PathVariable int movieId){
-        moviesRepository.deleteById(movieId);
+    public void deleteById(@PathVariable int movieId) throws IOException {
+        try {
+            moviesRepository.deleteById(movieId);
+        } catch(EmptyResultDataAccessException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No matching movie with ID: " + movieId);
+        }
     }
 
 }
